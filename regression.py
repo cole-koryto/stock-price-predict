72bacc09bf0fd11295798bba501e9a623fd61729
Cole <colekoryto17@gmail.com>
2025-02-26 23:10:52 -0500

    Add ML models

---
+++ regression.py
@@ -0,0 +1,175 @@
+from joblib import dump
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+
+
+def main():
+    prices_df = pd.read_csv("modified-prices-split-adjusted.csv")
+
+    # Drops rows without target stock
+    prices_df.drop(prices_df[prices_df["symbol"] != "F"].index, inplace=True)
+
+    # cleans dataset and removes unwanted features
+    prices_df = prices_df.dropna()
+
+    # splits data into training and test set
+    x_train, x_test, y_train, y_test = train_test_split(prices_df[["open", "low", "high", "volume", "sma_10", "macd", "rsi"]], prices_df["close"], test_size = 0.10, shuffle=False)
+
+    # scales data with standard scaler
+    x_train, x_test = scaleData(x_train, x_test)
+    print(x_train)
+
+    lin_reg = LinearRegression().fit(x_train, y_train)
+    y_train_pred = lin_reg.predict(x_train)
+    outputMetrics(y_train, y_train_pred)
+
+    # Creates and runs linear OLS model
+    createOLSModel(x_train, x_test, y_train, y_test)
+
+    # Creates and runs elastic model
+    createElasticModel(x_train, x_test, y_train, y_test)
+
+    # Creates and runs XGBoost model
+    createXGBoostModel(x_train, x_test, y_train, y_test)
+
+# scales data with standard scaler
+def scaleData(x_train, x_test):
+    # Creates and trains scalar
+    std_scaler = StandardScaler()
+    std_scaler.fit(x_train)
+
+    # Transforms data based on scalar
+    columns = x_train.columns
+    x_train = std_scaler.transform(x_train)
+    x_train = pd.DataFrame(x_train, columns=columns)
+    x_test = std_scaler.transform(x_test)
+    x_test = pd.DataFrame(x_test, columns=columns)
+
+    # saves scaler
+    dump(std_scaler, 'TrainingScaler.joblib')
+
+    return x_train, x_test
+
+
+# creates, tests, and visualizes a linear regression (OLS regression)
+def createOLSModel(x_train, x_test, y_train, y_test):
+
+    # creates a linear regression
+    print("\nCreating linear regression model (OLS regression)")
+    linearReg = LinearRegression(n_jobs=-1)
+    linearReg.fit(x_train, y_train)
+    print(f"Best model coefficients: {linearReg.coef_}")
+    print("\nTraining Set Metrics")
+    y_train_pred = linearReg.predict(x_train)
+    outputMetrics(y_train, y_train_pred)
+    print("\nTest Set Metrics")
+    y_test_pred = linearReg.predict(x_test)
+    outputMetrics(y_test, y_test_pred)
+
+    # plots a graph comparing actual value versus predicted value
+    # fig, ax = plt.subplots()
+    # y_pred = linearRegElastic.predict(x_test)
+    # ax.scatter(y_pred, y_test, edgecolors=(0, 0, 1), alpha=0.1)
+    # ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=3)
+    # ax.set_xlabel('Predicted')
+    # ax.set_ylabel('Actual')
+    # plt.show()
+
+    # saves model
+    dump(linearReg, 'Linear.joblib')
+
+# creates, tests, and visualizes a linear regression (elastic net regression)
+def createElasticModel(x_train, x_test, y_train, y_test):
+
+    # creates a linear regression
+    print("\nCreating linear regression model (elastic net)")
+    parameters = [{'l1_ratio': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]}]
+    K = 5
+    linearElasticRegElastic = GridSearchCV(ElasticNet(random_state=17, max_iter=10000), parameters, cv=K, verbose=0, n_jobs=-1)
+    linearElasticRegElastic.fit(x_train, y_train)
+    print(f"Best model parameters: {linearElasticRegElastic.best_params_}")
+    print(f"Best model coefficients: {linearElasticRegElastic.best_estimator_.coef_}")
+    print("\nTraining Set Metrics")
+    y_train_pred = linearElasticRegElastic.predict(x_train)
+    outputMetrics(y_train, y_train_pred)
+    print("\nTest Set Metrics")
+    y_test_pred = linearElasticRegElastic.predict(x_test)
+    outputMetrics(y_test, y_test_pred)
+
+    # plots a graph comparing actual value versus predicted value
+    # fig, ax = plt.subplots()
+    # y_pred = linearElasticRegElastic.predict(x_test)
+    # ax.scatter(y_pred, y_test, edgecolors=(0, 0, 1), alpha=0.1)
+    # ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=3)
+    # ax.set_xlabel('Predicted')
+    # ax.set_ylabel('Actual')
+    # plt.show()
+
+    # saves model
+    dump(linearElasticRegElastic, 'Elastic.joblib')
+    
+    
+# creates, tests, and visualizes a XGBoost regression
+def createXGBoostModel(x_train, x_test, y_train, y_test):
+    # creates a SVM polynomial model
+    print("\nCreating XGBoost regression model")
+    parameters = [{}]
+    K = 5
+    xgb_reg = GridSearchCV(XGBRegressor(), parameters, cv=K, verbose=0, n_jobs=-1)
+    xgb_reg.fit(x_train, y_train)
+    print(f"Best model parameters: {xgb_reg.best_params_}")
+    print("\nTraining Set Metrics")
+    y_train_pred = xgb_reg.predict(x_train)
+    outputMetrics(y_train, y_train_pred)
+    print("\nTest Set Metrics")
+    y_test_pred = xgb_reg.predict(x_test)
+    outputMetrics(y_test, y_test_pred)
+
+    # plots a graph comparing actual value versus predicted value
+    # fig, ax = plt.subplots()
+    # y_pred = xgb_reg.predict(x_test)
+    # ax.scatter(y_pred, y_test, edgecolors=(0, 0, 1), alpha=0.1)
+    # ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=3, )
+    # ax.set_xlabel('Predicted')
+    # ax.set_ylabel('Actual')
+    # plt.show()
+
+    # plot feature importance
+    # sorted_idx = xgb_reg.best_estimator_.feature_importances_.argsort()
+    # sorted_df = pd.DataFrame(sorted_idx, columns=x_train.columns)
+    # plt.barh(sorted_df)
+    # plt.xlabel("Xgboost Feature Importance")
+    # plot_importance(xgb_reg.best_estimator_)
+    # plt.show()
+    # feature_important = xgb_reg.best_estimator_.get_booster().get_score(importance_type='weight')
+    # keys = list(feature_important.keys())
+    # values = list(feature_important.values())
+    #
+    # data = pd.DataFrame(data=values, index=keys, columns=["score"]).sort_values(by="score", ascending=False)
+    # data.nlargest(40, columns="score").plot(kind='barh', figsize=(20, 10))  ## plot top 40 features
+    # plt.show()
+
+    # saves model
+    dump(xgb_reg, 'XGBR.joblib')
+
+
+# outputs metrics for given predictions and actual data set
+def outputMetrics(y_actual, y_pred):
+    mse = metrics.mean_squared_error(y_actual, y_pred)
+    r2 = metrics.r2_score(y_actual, y_pred)
+    print("--------------------------------------")
+    print('MSE is {}'.format(mse))
+    print('R2 score is {}'.format(r2))
+    print("--------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
